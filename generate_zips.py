d6a80f117a987f345218ff1817a8cfb40d6c2a70
Anne Lhote <anne.lhote@gmail.com>
2016-01-15 16:58:56 +0100

    zip the folder

--- generate_zips.py
+++ generate_zips.py
@@ -2,27 +2,13 @@
 # -*- coding: utf-8 -*-
 # Execution example : python generate_zips.py "path/to/folder/to/zip"
 
-import os, sys, zipfile
+import os, sys, zipfile, zlib
 
-# def zipdir(path, ziph) :
-# 	# ziph is zipfile handle
-# 	# for root, dirs, files in os.walk(path):
-# 		# for file in files:
-# 			# ziph.write(os.path.join(root, file))
-# 	assert os.path.isdir(path)
-# 	with closing(ZipFile(archivename, "w", ZIP_DEFLATED)) as z:
-# 		for root, dirs, files in os.walk(basedir):
-# 			# NOTE: ignore empty directories
-# 			for fn in files:
-# 				absfn = os.path.join(root, fn)
-# 				zfn = absfn[len(basedir)+len(os.sep):] #XXX: relative path
-# 				z.write(absfn, zfn)
-
-def zipdir(path, ziph):
+def zipdir(path, ziph) :
 	# ziph is zipfile handle
-	for root, dirs, files in os.walk(path):
-		for file in files:
-			ziph.write(os.path.join(root, file))
+	for root, dirs, files in os.walk(path) :
+		for file in files :
+			ziph.write(os.path.join(root, file), os.path.join(root.replace(path, ''), file))
 
 if __name__ == '__main__' :
 	if len(sys.argv) != 2 :
@@ -31,10 +17,8 @@ if __name__ == '__main__' :
 		print 'Correct usage : python ' + sys.argv[0] + ' "path/to/folder/to/zip"'
 		print 'The first argument is mandatory and is the path to the folder to zip'
 	else :
-		zf = zipfile.ZipFile('python.zip', mode='w', allowZip64=1)
-		try :
-			print 'adding archive folder'
-			zf.write(sys.argv[1])
-		finally :
-			print 'closing'
-			zf.close()
+		zf = zipfile.ZipFile('sp5-ol.zip', mode='w', compression=zipfile.ZIP_DEFLATED, allowZip64=1)
+		print 'Adding archive folder'
+		zipdir(sys.argv[1], zf)
+		print 'Closing'
+		zf.close()
