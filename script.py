5e48cedf3ca0fe92e69d52adf00f9578b696f6e8
Anne L'Hôte <anne.lhote@gmail.com>
2017-01-17 17:26:38 +0100

    Add logs

--- script.py
+++ script.py
@@ -6,11 +6,27 @@
 # Libs
 #
 
+import logging
+import os
 import sys
+
+
+#
+# Config
+#
+
+log_folder = 'log'
+log_level = logging.DEBUG
+
 
 #
 # Main
 #
 
 if __name__ == '__main__' :
+	# Generate log file path
+	log_file = os.path.join(log_folder, sys.argv[0].replace('.py', '.log'))
+	# Init logs
+	logging.basicConfig(filename=log_file, filemode='a+', format='%(asctime)s  |  %(levelname)s  |  %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=log_level)
+	logging.info('Start script')
 	sys.exit()
