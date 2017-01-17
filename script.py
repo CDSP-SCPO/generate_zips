cb581b9af33f3c519aa9989885a7af2b7a702749
Anne L'Hôte <anne.lhote@gmail.com>
2017-01-17 17:20:09 +0100

    Init script

---
+++ script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Execution example : python script.py "path/to/folder/to/convert" "path/to/matching/file"
+
+#
+# Libs
+#
+
+import sys
+
+#
+# Main
+#
+
+if __name__ == '__main__' :
+	sys.exit()
